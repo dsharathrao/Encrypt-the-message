cc7265021fd2d2e25ecc6544955a647505fb9899
D Sharath Kumar Rao <50975866+dsharathrao@users.noreply.github.com>
2019-08-26 19:07:47 +0530

    Decrypt the message

---
+++ decrypt.py
@@ -0,0 +1,11 @@
+alphabet='abcdefghijklmnopqrstuvwxyz_'  
+key = 3  
+newmsg=""  
+message = input("Enter Your Message : ")  
+for character in message:  
+     position = alphabet.find(character)  
+     newposition = (position-key)%27  
+     newchar = alphabet[newposition]  
+     print('Encrypted new character is :',newchar)  
+     newmsg+=newchar  
+print('The encrypted message is ', newmsg)  
